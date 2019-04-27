7cecaaae09549e964bbaffbd4c0d248498134976
monst <monster240798@gmail.com>
2019-04-27 15:55:09 +0300

    Commit#3 create Budget implementation

--- src/main/java/com/smartcity/DAO/BudgetDAO.java
+++ src/main/java/com/smartcity/DAO/BudgetDAO.java
@@ -1,4 +1,12 @@
 package com.smartcity.DAO;
 
+import com.smartcity.domain.Budget;
+
 public interface BudgetDAO {
+
+    Budget save(Budget budget);
+
+    Budget findById(Integer id);
+
+    boolean deleteById(Integer id);
 }
